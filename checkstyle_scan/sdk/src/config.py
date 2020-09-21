740e4d4c6f66814747fe48feb35d798448f72b9b
jimxzcai <jimxzcai@tencent.com>
2020-09-21 11:38:50 +0800

    feat: 添加checkstyle工具脚步及镜像Dockerfile#1

---
+++ checkstyle_scan/sdk/src/config.py
@@ -0,0 +1,100 @@
+﻿import os, sys
+import json
+import re
+import xml.etree.ElementTree as ET
+
+properties_info={}
+current_path = ''
+config_path = ''
+third_rules = ''
+tencent_checker_style='com.tencent.checks.'
+if 'python' in sys.executable:
+    current_path = sys.path[0]
+else:
+    current_path = os.path.dirname(os.path.realpath(sys.executable))
+
+config_path = current_path+'/../config'
+third_rules = current_path+'/../../third_rules'
+
+rule_config_file = os.path.join(config_path, 'tencent_config.xml')
+
+def update_rule_config(properties_info):
+    try:
+        #get the config
+        tree = ET.ElementTree(file=rule_config_file)
+        root = tree.getroot()
+        
+        #get the open checker and option
+        checkers_list = ['SuppressWarningsHolder']
+        checker_options = {}
+        if 'openCheckers' in properties_info:
+            for checker in properties_info['openCheckers']:
+                if 'checkerName' in checker and 'nativeChecker' in checker:
+                        if checker['nativeChecker'] == True:
+                            checkers_list.append(checker['checkerName'])
+                        else:
+                            checkers_list.append(tencent_checker_style+''+checker['checkerName'])
+                if 'checkerOptions' in checker:
+                    options = {}
+                    for option in checker['checkerOptions']:
+                        if 'checkerOptionName' in option and 'checkerOptionValue' in option:
+                            options[option['checkerOptionName']] = option['checkerOptionValue']
+                    if checker['nativeChecker'] == True:
+                        checker_options[checker['checkerName']] = json.dumps(options)
+                    else:
+                        checker_options[tencent_checker_style+''+checker['checkerName']] = json.dumps(options)
+        
+        #delete skip_checkers && add market rules
+        for module in root.findall('module'):
+            if module.attrib['name'] != 'TreeWalker' and not module.attrib['name'] in checkers_list and not re.search('Filter$', str(module.attrib['name'])):
+                root.remove(module)
+            elif module.attrib['name'] == 'TreeWalker':
+                for tree_walker_module in module.findall('module'):
+                    if not tree_walker_module.attrib['name'] in checkers_list and not re.search('Filter$', str(tree_walker_module.attrib['name'])):
+                        module.remove(tree_walker_module)
+
+        #update checker option
+        for module in root.findall('module'):
+            if module.attrib['name'] != 'TreeWalker' and module.attrib['name'] in checkers_list and module.attrib['name'] in checker_options:
+                option_list =  json.loads(checker_options[module.attrib['name']])
+                keys = option_list.keys()
+                for key in keys:
+                    if key == 'tokens':
+                        if isinstance(option_list[key], list):
+                            for token in option_list[key]:
+                                ET.SubElement(module, 'property', attrib={'name':key, 'value':token})
+                        else:
+                            ET.SubElement(module, 'property', attrib={'name':key, 'value':option_list[key]})
+                    else:
+                        for prop in module.findall('property'):
+                            if key == prop.attrib['name']:
+                                prop.attrib['value'] = option_list[key]
+                                break
+                        else:
+                            ET.SubElement(module, 'property', attrib={'name':key, 'value':option_list[key]})
+            elif module.attrib['name'] == 'TreeWalker':
+                for tree_walker_module in module.findall('module'):
+                    if tree_walker_module.attrib['name'] in checkers_list and tree_walker_module.attrib['name'] in checker_options:
+                        option_list =  json.loads(checker_options[tree_walker_module.attrib['name']])
+                        keys = option_list.keys()
+                        for key in keys:
+                            if key == 'tokens':
+                                if isinstance(option_list[key], list):
+                                    for token in option_list[key]:
+                                        ET.SubElement(tree_walker_module, 'property', attrib={'name':key, 'value':token})
+                                else:
+                                    ET.SubElement(tree_walker_module, 'property', attrib={'name':key, 'value':option_list[key]})
+                            else:
+                                for prop in tree_walker_module.findall('property'):
+                                    if key == prop.attrib['name']:
+                                        prop.attrib['value'] = option_list[key]
+                                        break
+                                else:
+                                    ET.SubElement(tree_walker_module, 'property', attrib={'name':key, 'value':option_list[key]})
+        with open(rule_config_file, 'wb') as file:
+            file.write('<?xml version="1.0" encoding="UTF-8" ?><!DOCTYPE module PUBLIC "-//Checkstyle//DTD Checkstyle Configuration 1.3//EN" "https://checkstyle.org/dtds/configuration_1_3.dtd">'.encode('utf8'))
+            tree.write(file, 'utf-8')
+
+    except Exception as e:
+        raise Exception(e)
+        
---
+++ checkstyle_scan/sdk/src/scan.py
@@ -0,0 +1,136 @@
+﻿import sys
+import os,re
+import json
+import subprocess
+import config
+import re
+import traceback
+import xml.etree.ElementTree as ET
+
+options = {}
+
+def check_path_match_skip(file_path, skip_path_list):
+    file_path = file_path.replace('\\', '/')
+    no_skip = True
+    try:
+        for skip in skip_path_list:
+            if re.search(skip, file_path):
+                return False
+    except:
+        pass
+    return no_skip
+    
+def get_classpath(root_dir):
+    class_paths = []
+    for root, dir, files in os.walk(root_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if file_path.endswith('.jar'):
+                class_paths.append(file_path)
+    return ':'.join(class_paths)
+
+def scan(filename, config_path, third_rules, skip_path_list):
+    file_defects = []
+    skip_option = ''
+    result_xml = config.current_path+'/result.xml'
+    class_path = get_classpath(config.current_path+'/../../tool/')
+    for skip_path in skip_path_list:
+        if '' != skip_path and not '$' in skip_path:
+            skip_option += ' -x \"'+skip_path+'\"'
+    cmd = "java -classpath %s com.puppycrawl.tools.checkstyle.Main %s -c %s -f xml \"%s\" -o \"%s\"" % (class_path, skip_option, config.rule_config_file, filename, result_xml)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,shell=True,start_new_session=True)
+    try:
+        for line in p.stdout:
+            pass
+    finally:
+        p.terminate()
+        p.wait()
+    
+    if os.path.isfile(result_xml):
+        try:
+            tree = ET.ElementTree(file=result_xml)
+            for elem in tree.iter():
+                if elem.tag == "file":
+                    file_path = elem.attrib['name']
+                    if not file_path.endswith('.java'):
+                        continue
+                    if not check_path_match_skip(file_path, skip_path_list):
+                        continue
+                    for sub_elem in elem.iter():        
+                        if sub_elem.tag == "error":
+                            defect = {}
+                            defect['filePath'] = file_path
+                            defect['line'] = sub_elem.attrib['line']
+                            if not 'onesdk.' in sub_elem.attrib['source'] and len(sub_elem.attrib['source'].rsplit('.', 1)) > 1:
+                                short_checker = sub_elem.attrib['source'].rsplit('.', 1)[1]
+                                if short_checker.endswith('Check'):
+                                    defect['checkerName'] = short_checker[:-5]
+                            else:
+                                defect['checkerName'] = sub_elem.attrib['source']
+                            defect['description'] = sub_elem.attrib['message']
+                            if defect != {}:
+                                file_defects.append(defect)
+        except Exception:
+            err_msg = "---------------%s---------------\n%s\n%s\n" % (filename, result_xml, traceback.format_exc())
+            print(err_msg)
+    
+        #clean result
+        os.remove(result_xml)
+    return file_defects
+
+if __name__ == "__main__" :
+    if len(sys.argv) > 2:
+        input_data = {}
+        output_data = {}
+        skip_path_list = []
+        all_file_defects = []
+        for i in range(len(sys.argv)-1):
+            if not "=" in sys.argv[i+1] or not "--" in sys.argv[i+1]:
+                print("Usage %s --xxx=xxx" % sys.argv[0])
+                sys.exit()
+        for i in range(len(sys.argv)-1):
+            tmp = sys.argv[i+1].split("=",1)
+            options[tmp[0].replace("--","")] = str(tmp[1]).strip()
+        if 'input' in options and os.path.isfile(options['input']):
+            #获取input数据
+            with open(options['input'], 'r', encoding='utf-8') as file:
+                input_data = json.load(file) 
+
+            if os.path.isfile(config.rule_config_file):
+                #更新规则配置文件
+                config.update_rule_config(input_data)
+                
+                #获取过滤路径列表
+                if 'skipPaths' in input_data:
+                    skip_path_list = input_data['skipPaths']
+                
+                scan_path_list = []
+                #增量扫描
+                if 'scanType' in input_data and input_data['scanType'] == 'increment':
+                    for file_path in input_data['incrementalFiles']:
+                        if 'whitePathList' in input_data and len(input_data['whitePathList']) > 0 and check_path_match_skip(file_path, input_data['whitePathList']):
+                            continue
+                        scan_path_list.append(file_path)
+                #全量扫描
+                elif 'scanType' in input_data and input_data['scanType'] == 'full':
+                    if 'whitePathList' in input_data and len(input_data['whitePathList']) > 0:
+                        for white_path in input_data['whitePathList']:
+                            if os.path.isdir(white_path) or os.path.isfile(white_path):
+                                scan_path_list.append(white_path)
+                    elif 'scanPath' in input_data and os.path.isdir(input_data['scanPath']):
+                        scan_path_list.append(input_data['scanPath'])
+                
+                for scan_path in scan_path_list:
+                    file_defects = scan(scan_path, config.rule_config_file, config.third_rules, skip_path_list)
+                    if len(file_defects) > 0:
+                        all_file_defects.extend(file_defects) 
+                    
+            output_data['defects'] = all_file_defects
+            if 'output' in options:
+                with open(options['output'], 'w') as file:
+                    print('generate output json file: '+options['output'])
+                    file.write(json.dumps(output_data, sort_keys=True, indent=4))
+    else:
+         print("Usage %s --xxx=xxx" % sys.argv[0])
+         print('--input: the file path of input the json file for tool to scan')
+         print('--output the file path of output the result')
