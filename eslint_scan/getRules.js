e76b84dc03288f19e7766d69b4198d4e7c25c41f
brooklin <brooklin@tencent.com>
2021-07-13 19:57:18 +0800

    fix: 修复eslint扫描报错

--- eslint_scan/getRules.js
+++ eslint_scan/getRules.js
@@ -1,7 +1,7 @@
-const base = require('@tencent/eslint-config-tencent/base')
+const base = require('eslint-config-tencent/base')
 const jsRules = base.rules
 
-const ts = require('@tencent/eslint-config-tencent/ts')
+const ts = require('eslint-config-tencent/ts')
 const tsRules = {...jsRules, ...ts.rules}
 
 // console.log(JSON.stringify(jsRules))
